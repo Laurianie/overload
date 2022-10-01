29e6484283c2daf3ff298cf9e44d29b04fd599df
laurianie guiteau <laurianie_guiteau24@milton.edu>
2022-10-01 18:41:16 -0400

    Initial commit

---
+++ src/systemOverload.java
@@ -0,0 +1,19 @@
+public class systemOverload {
+
+    public systemOverload (int crash,boolean fix){
+        return;
+        }
+
+    public systemOverload(boolean fix, int crash) {
+        return;
+    }
+
+    public static void main(String[] args) {
+        int crash = 5;
+        boolean fix = false;
+        System.out.println("There has been 5 laptops that have crashed and no one has come to fix them yet");
+    }
+}
+
+
+
